c89e294d4fae91c53217c69342fe9ce64a32847e
daffamf <daffamuhammadfirdaus5@gmail.com>
2021-07-28 16:11:45 +0700

    modified user

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ import { Text, View,ListItem } from 'react-native';
 import Icon from 'react-native-vector-icons/dist/MaterialCommunityIcons'
 import {BottomNavigation,Avatar } from 'react-native-paper';
 import { Calendar } from 'react-native-calendario';
+import MapView from 'react-native-maps';
 
 import './styles'
 
@@ -75,8 +76,6 @@ const CalenderRoute = () =>
 
 const UserRoute = () =>
  <View>
- 
-
 </View>;
 
 
